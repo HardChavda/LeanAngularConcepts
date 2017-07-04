ad7f5c037ec1d7e56afca2c0bc9851b867ef617e
chavdahard <hard@leafnode.io>
2017-07-04 14:52:46 +0530

    Routing and basic material design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {ServerComponent} from "./Server/server.component";
+import {MdButtonModule, MdCardModule, MdIconModule, MdMenuModule, MdToolbarModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Routes, RouterModule} from "@angular/router";
 
+const appRoutes: Routes = [
+  {path:"server", component: ServerComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,14 @@ import {ServerComponent} from "./Server/server.component";
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MdButtonModule,
+    MdCardModule,
+    MdIconModule,
+    MdMenuModule,
+    MdToolbarModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
